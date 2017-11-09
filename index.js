ed2b776b9f94e49bceec7459060259a84d88d67a
Cara Kane <cara.kane@gmail.com>
2017-11-09 17:28:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Code your solution in this file
 
 function findMatching(drivers, string) {
-  
+  return drivers.filter(driver => driver === string)
 }
 
 function fuzzyMatch(drivers, string) {
-  
+
 }
 
 function matchName(drivers, string) {
-  
+
 }
