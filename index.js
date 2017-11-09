834fbdff5c0106d9f2af768e71cb5ba25c7c357b
Cara Kane <cara.kane@gmail.com>
2017-11-09 17:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(drivers, string) {
-  return drivers.filter(driver => driver == string)
+  return drivers.filter(driver => driver.toUppercase() === string.toUpperCase())
 }
 
 function fuzzyMatch(drivers, string) {
